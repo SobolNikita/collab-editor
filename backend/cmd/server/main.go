f16904173f71c2834779bda98ccdf78d0b3a9d37
Sobol Nikita <soboln2006@gmail.com>
2026-02-21 16:52:25 +0300

    feat: add config, cors

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/SobolNikita/collab-editor/internal/config"
+	"github.com/SobolNikita/collab-editor/internal/middleware"
+)
+
+func main() {
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("Failed to get database URL: %v", err)
+	}
+	db, err := config.ConnectDB(&cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello, Go!")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, Go!"))
+	})
+
+	addr := ":" + cfg.HTTPPort
+
+	log.Println("Starting server at port ", cfg.HTTPPort)
+	err = http.ListenAndServe(addr, middleware.CORS(mux))
+	if err != nil {
+		log.Fatal("Error starting the server:", err)
+	}
+}
--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"database/sql"
+	"errors"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	HTTPPort    string
+}
+
+func GetConfig() (Config, error) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		return Config{}, err
+	}
+	connStr := os.Getenv("DATABASE_URL")
+	httpPort := os.Getenv("HTTP_PORT")
+	if connStr == "" {
+		return Config{}, errors.New("DATABASE_URL is not set")
+	}
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+	return Config{
+		DatabaseURL: connStr,
+		HTTPPort:    httpPort,
+	}, nil
+}
+
+func ConnectDB(cfg *Config) (*sql.DB, error) {
+	db, err := sql.Open("pgx", cfg.DatabaseURL)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
